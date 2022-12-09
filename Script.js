bde954b7ff212c94a5739fb82881e3c54a79b080
rwillowb <114246902+rwillowb@users.noreply.github.com>
2022-12-08 23:57:15 -0500

    removing video

--- Script.js
+++ Script.js
@@ -73,21 +73,3 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
-
-/* Video*/
-// Get the video
-var video = document.getElementById("myVideo");
-
-// Get the button
-var btn = document.getElementById("myBttn");
-
-// Pause and play the video, and change the button text
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
