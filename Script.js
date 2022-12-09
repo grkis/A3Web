f6521eeeb45e3e035421d9a58e666388d79d0706
grkis <grki@flemingcollege.ca>
2022-12-09 08:12:04 -0500

    gallery stuff

--- Script.js
+++ Script.js
@@ -81,17 +81,3 @@ function TabGallery(imgs) {
   imgText.innerHTML = imgs.alt;
   expandImg.parentElement.style.display = "block";
 }
-
-// Gallery JS
-function myFunction(imgs) {
-  // Get the expanded image
-  var expandImg = document.getElementById("expandedImg");
-  // Get the image text
-  var imgText = document.getElementById("imgtext");
-  // Use the same src in the expanded image as the image being clicked on from the grid
-  expandImg.src = imgs.src;
-  // Use the value of the alt attribute of the clickable image as text inside the expanded image
-  imgText.innerHTML = imgs.alt;
-  // Show the container element (hidden with CSS)
-  expandImg.parentElement.style.display = "block";
-}
