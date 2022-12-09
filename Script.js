778c33543998d5f036f2d4908ef6788f540e4e79
rwillowb <114246902+rwillowb@users.noreply.github.com>
2022-12-08 23:37:51 -0500

    vid.2

--- Script.js
+++ Script.js
@@ -73,3 +73,21 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+/* Video*/
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
